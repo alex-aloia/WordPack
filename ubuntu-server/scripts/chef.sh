cff19fe4c2895dd59a11bd5b004ca66a9fe3bc68
Alex Aloia <tripl3infinity@gmail.com>
2015-05-13 17:45:11 -0500

    cleaning/moving ansible/packer folder

--- ubuntu-server/scripts/chef.sh
+++
@@ -1,2 +0,0 @@
-# install Omnibus Chef Client
-wget -O - http://opscode.com/chef/install.sh | sudo bash -s
--- ubuntu-server/scripts/ruby.sh
+++
@@ -1,2 +0,0 @@
-# Install Ruby from packages
-apt-get -y install ruby1.9.1 ruby1.9.1-dev libruby1.9.1 ri
